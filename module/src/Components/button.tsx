58cae23180f73a07c136123efd01ae7406a3cf49
George Abreu Jr. <george8794@gmail.com>
2025-04-30 19:08:51 +0000

    add disabled mode for buttons

--- module/src/Components/button.tsx
+++ module/src/Components/button.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-export default function Button({ children, onClick, styles }: {
+export default function Button({ children, onClick, styles, disabled }: {
     children: React.ReactNode,
     onClick?: () => void,
     styles?: React.CSSProperties,
+    disabled?: boolean
 }): React.ReactElement {
     const buttonStyles: React.CSSProperties = {
         padding: '0.5rem 1rem',
-        backgroundColor: 'var(--primary-color)',
-        color: 'var(--text-color)',
+        backgroundColor: disabled ? 'var(--disabled-color)' : 'var(--primary-color)',
+        color: disabled ? 'var(--disabled-text-color)' : 'var(--text-color)',
         border: '1px solid var(--secondary-color)',
-        cursor: 'pointer',
+        cursor: disabled ? 'not-allowed' : 'pointer',
         borderRadius: '0.25rem',
         ...styles,
     };
 
-    return <button style={buttonStyles} onClick={onClick}>{children}</button>;
+    return <button style={buttonStyles} onClick={onClick} disabled={disabled}>{children}</button>;
 }
